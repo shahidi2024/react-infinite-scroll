390716c2603937550465022d77bd6a91da235cc5
Ali Shahidi <a.shahidi2020@yahoo.com>
2024-12-30 22:09:43 +0330

    Add tailwind css to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import { useState } from "react";
-import "./App.css";
-
-function App() {
-  return <></>;
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline text-red-300">
+      Hello world!
+    </h1>
+  )
 }
-
-export default App;
